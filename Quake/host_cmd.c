fc883c08b289c407ac884bd814aa5be4be93e82a
Andrei Drexler <makro@rq3.com>
2024-10-12 13:48:23 +0200

    Ignore parenthesis arguments passed to `setpos`

--- Quake/host_cmd.c
+++ Quake/host_cmd.c
@@ -1773,6 +1773,9 @@ adapted from fteqw, originally by Alex Shadowalker
 */
 static void Host_SetPos_f(void)
 {
+	int		i, numargs;
+	float	args[6];
+
 	if (cmd_source == src_command)
 	{
 		Cmd_ForwardToServer ();
@@ -1782,7 +1785,16 @@ static void Host_SetPos_f(void)
 	if (pr_global_struct->deathmatch)
 		return;
 
-	if (Cmd_Argc() != 7 && Cmd_Argc() != 4)
+	for (i = 1, numargs = 0; i < Cmd_Argc (); i++)
+	{
+		const char *str = Cmd_Argv (i);
+		if (strcmp (str, "(") == 0 || strcmp (str, ")") == 0)
+			continue;
+		if (++numargs <= 6)
+			args[numargs - 1] = atof (str);
+	}
+
+	if (numargs != 6 && numargs != 3)
 	{
 		SV_ClientPrintf("usage:\n");
 		SV_ClientPrintf("   setpos <x> <y> <z>\n");
@@ -1809,19 +1821,19 @@ static void Host_SetPos_f(void)
 	sv_player->v.velocity[0] = 0;
 	sv_player->v.velocity[1] = 0;
 	sv_player->v.velocity[2] = 0;
-	
-	sv_player->v.origin[0] = atof(Cmd_Argv(1));
-	sv_player->v.origin[1] = atof(Cmd_Argv(2));
-	sv_player->v.origin[2] = atof(Cmd_Argv(3));
-	
-	if (Cmd_Argc() == 7)
+
+	sv_player->v.origin[0] = args[0];
+	sv_player->v.origin[1] = args[1];
+	sv_player->v.origin[2] = args[2];
+
+	if (numargs == 6)
 	{
-		sv_player->v.angles[0] = atof(Cmd_Argv(4));
-		sv_player->v.angles[1] = atof(Cmd_Argv(5));
-		sv_player->v.angles[2] = atof(Cmd_Argv(6));
+		sv_player->v.angles[0] = args[3];
+		sv_player->v.angles[1] = args[4];
+		sv_player->v.angles[2] = args[5];
 		sv_player->v.fixangle = 1;
 	}
-	
+
 	SV_LinkEdict (sv_player, false);
 }
 
