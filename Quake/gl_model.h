4a41a7e9429e98e44c873616afcebe2e84f52aa2
Andrei Drexler <makro@rq3.com>
2024-11-09 18:33:06 +0100

    Remove unused types

--- Quake/gl_model.h
+++ Quake/gl_model.h
@@ -297,21 +297,6 @@ typedef struct
 	char				name[16];
 } maliasframedesc_t;
 
-typedef struct
-{
-	trivertx_t			bboxmin;
-	trivertx_t			bboxmax;
-	int					frame;
-} maliasgroupframedesc_t;
-
-typedef struct
-{
-	int						numframes;
-	int						intervals;
-	maliasgroupframedesc_t	frames[1];
-} maliasgroup_t;
-
-
 #define	MAX_SKINS	32
 typedef struct {
 	int			ident;
